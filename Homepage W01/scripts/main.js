cfb3cd3734ec25e752779ef80607e169bbe52000
BlakeMauri01 <Blake_mauri@byui.edu>
2019-09-23 10:53:46 -0600

    rev 4

---
+++ Homepage W01/scripts/main.js
@@ -0,0 +1,4 @@
+//Get the current date for website copyright year
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
