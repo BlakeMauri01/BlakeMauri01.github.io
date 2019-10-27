2950c60508baeb4db28f8c11f06006e7e06626ca
BlakeMauri01 <Blake_mauri@byui.edu>
2019-10-26 23:33:52 -0600

    3.0.2

---
+++ lesson05/scripts/windchill.js
@@ -0,0 +1,17 @@
+let currentTemp = 41;
+let windSpeed =8;
+let windChill =0;
+
+if (currentTemp < 50 && windSpeed >3){
+    windChill =35.74 + 0.6215 *currentTemp -35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * currentTemp * Math.pow(windSpeed, 0.16);
+}
+
+document.getElementById("currentTemp").innerHTML = currentTemp + '&deg';
+document.getElementById("windSpeed").innerHTML = windspeed + ' mph';
+
+if (windChill == 0){
+    document.getElementById("windCHill").innerHTML = 'N/A'
+}
+else{
+    document.getElementById("windChill").innerHTML = Math.round(windChill) + '&deg;';
+}
