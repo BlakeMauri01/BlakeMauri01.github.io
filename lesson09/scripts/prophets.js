8687890967c90ea75379a2c8f72d43982a13d63e
BlakeMauri01 <Blake_mauri@byui.edu>
2019-11-11 15:09:43 -0700

    10

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,10 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+});
+
