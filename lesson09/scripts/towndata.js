b9d5783d798fcd887a72758698b81b65c8ad6d46
BlakeMauri01 <Blake_mauri@byui.edu>
2019-11-21 23:42:02 -0700

    rev 9

---
+++ lesson09/scripts/towndata.js
@@ -0,0 +1,110 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(response => {
+        response.json()
+            .then(
+                response => {
+                    const towns = response.towns;
+
+                    towns.forEach(
+                        (town) => {
+                            if (town.name.toLowerCase() == "fish haven" || town.name.toLowerCase() == "preston" || town.name.toLowerCase() == "soda springs"){
+                            let article = document.createElement('article');
+                            let h2 = document.createElement('h2');
+
+                            //Images
+                            let imageDiv = document.createElement('div');
+                            let image = document.createElement('img');
+
+                            image.setAttribute('src', 'images/' + town.photo);
+
+                            imageDiv.appendChild(image);
+                            article.appendChild(imageDiv);
+
+                            //Names
+                            image.setAttribute('alt', town.name);
+
+                            h2.textContent = town.name;
+
+                            article.appendChild(h2);
+
+                            //Motto
+                            let mottoDiv = document.createElement('div');
+
+                            let mottoLabel = document.createElement('label');
+                            mottoLabel.textContent = 'Motto: ';
+
+                            let mottoSpan = document.createElement('span');
+                            mottoSpan.textContent = town.motto;
+
+                            mottoDiv.appendChild(mottoLabel);
+                            mottoDiv.appendChild(mottoSpan);
+
+                            article.appendChild(mottoDiv);
+
+                            //Year Founded
+                            let yearFoundedDiv = document.createElement('div');
+                            let yearFoundedLabel = document.createElement('label');
+                            yearFoundedLabel.textContent = 'Year Founded: ';
+
+                            let yearFoundedSpan = document.createElement('span');
+                            yearFoundedSpan.textContent = town.yearFounded;
+
+                            yearFoundedDiv.appendChild(yearFoundedLabel);
+                            yearFoundedDiv.appendChild(yearFoundedSpan);
+
+                            article.appendChild(yearFoundedDiv);
+
+                            document.querySelector('section.towns').appendChild(article);
+
+                            //Current Population
+                            let currentPopulationDiv = document.createElement('div');
+                            let currentPopulationLabel = document.createElement('label');
+                            currentPopulationLabel.textContent = 'Current Population: ';
+
+                            let currentPopulationSpan = document.createElement('span');
+                            currentPopulationSpan.textContent = town.currentPopulation;
+
+                            currentPopulationDiv.appendChild(currentPopulationLabel);
+                            currentPopulationDiv.appendChild(currentPopulationSpan);
+
+                            article.appendChild(currentPopulationDiv);
+
+                            document.querySelector('section.towns').appendChild(article);
+
+                            //Average Rainfall
+                            let averageRainfallDiv = document.createElement('div');
+                            let averageRainfallLabel = document.createElement('label');
+                            averageRainfallLabel.textContent = 'Average Rainfall: ';
+
+                            let averageRainfallSpan = document.createElement('span');
+                            averageRainfallSpan.textContent = town.averageRainfall;
+
+                            averageRainfallDiv.appendChild(averageRainfallLabel);
+                            averageRainfallDiv.appendChild(averageRainfallSpan);
+
+                            article.appendChild(averageRainfallDiv);
+
+                            document.querySelector('section.towns').appendChild(article);
+
+                            //Events
+                            // let eventsDiv = document.createElement('div');
+                            // let eventsLabel = document.createElement('label');
+                            // eventsLabel.textContent = 'Events: ';
+
+                            // let eventsSpan = document.createElement('span');
+                            // eventsSpan.textContent = town.events.join(', ');
+
+                            // eventsDiv.appendChild(eventsLabel);
+                            // eventsDiv.appendChild(eventsSpan);
+
+                            // article.appendChild(eventsDiv);
+
+                            document.querySelector('section.towns').appendChild(article);
+                            }
+                        }
+                    );
+                }
+            )
+    });
---
+++ lesson10/scripts/api.js
@@ -0,0 +1,1 @@
+const apiURL = 'https://samples.openweathermap.org/data/2.5/weather?id=5604473&appid=b6907d289e10d714a6e88b30761fae22';
